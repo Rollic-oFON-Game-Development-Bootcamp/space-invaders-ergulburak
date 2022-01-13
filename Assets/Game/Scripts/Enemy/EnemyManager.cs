84ca092ab989f25f1e88b9eac468ca0f8fc28c6e
Burak Ergül <burakcan41@gmail.com>
2022-01-14 01:07:53 +0300

    +added: assets, ship controllers

---
+++ Assets/Game/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyManager : MonoBehaviour
+{
+  private Enemy[,] enemies = new Enemy[11, 5];
+
+  private void Awake()
+  {
+    for (int i = 0; i < enemies.Rank; i++)
+    {
+    }
+  }
+}
+
+public class Enemy
+{
+}
