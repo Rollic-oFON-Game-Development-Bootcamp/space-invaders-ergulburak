683fa2aa0a413dbf9d9d68f9446afc453a4fe1c7
Burak Ergül <burakcan41@gmail.com>
2022-01-14 23:23:33 +0300

    +added: enemy controller

--- Assets/Game/Scripts/Enemy/EnemyManager.cs
+++ Assets/Game/Scripts/Enemy/EnemyManager.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
 public class EnemyManager : MonoBehaviour
 {
-  private Enemy[,] enemies = new Enemy[11, 5];
+  [SerializeField] private Enemy enemyPrefab;
+  private List<Enemy> enemies = new List<Enemy>();
+  private BoxFormation formation => GetComponent<BoxFormation>();
+  private List<Vector3> enemyPoints = new List<Vector3>();
 
   private void Awake()
   {
-    for (int i = 0; i < enemies.Rank; i++)
+    enemyPoints = formation.EvaluatePoints().ToList();
+    for (int i = 0; i < enemyPoints.Count; i++)
     {
+      var enemy = Instantiate(enemyPrefab);
+      enemies.Add(enemy);
+      enemy.transform.position = enemyPoints[i]+transform.position;
     }
   }
-}
-
-public class Enemy
-{
 }
